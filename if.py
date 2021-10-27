a9c881a7e4e3c5c4e1d38752154491617be6a958
Kletsov <93253076+Kletsov@users.noreply.github.com>
2021-10-27 15:11:27 +0300

    Initial commit

---
+++ if.py
@@ -0,0 +1,11 @@
+
+def f(a, b, c):
+
+    if a>b:
+        return print(c)
+    elif a == b:
+        return print ('Крестьяне жалуются на налоги')
+    else:
+        return print('В казну поступило золото!')
+
+f(input(), input(), input())
---
+++ Конкатенация.py
@@ -0,0 +1,7 @@
+qty = int(input('Сколько досок?'))
+heigh = float(input('Какая высота доски?'))
+width = float(input('Какая ширина доски?'))
+
+area = (heigh + width) * qty
+
+print('Какая площадь досок', area, 'см/кв')
